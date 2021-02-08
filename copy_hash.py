67a1cb2fa07534e851313e93272aa25f3b9dbe26
Stefan Oderbolz <oderbolz@gmail.com>
2021-02-08 17:03:24 +0100

    Create copy_hash.py

---
+++ copy_hash.py
@@ -0,0 +1,43 @@
+import re
+import json
+import os
+import sys
+from ckanapi import RemoteCKAN, NotFound
+
+# load env
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
+# setup logging
+import logging
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+console = logging.StreamHandler()
+logging.getLogger('').addHandler(console)
+log = logging.getLogger(__name__)
+
+BASE_URL = os.getenv('CKAN_BASE_URL')
+API_KEY = os.getenv('CKAN_API_KEY')
+
+import requests
+import urllib3
+urllib3.disable_warnings()
+
+site = RemoteCKAN(BASE_URL, apikey=API_KEY)
+
+for dataset in sys.stdin:
+    data = {
+        "id": dataset.strip()
+    }
+    log.info('Copying zh_hash to hash in dataset %s...' % data['id'])
+    try:
+        ckan_dataset = site.call_action('package_show', data, requests_kwargs={'verify': False})
+        
+        for resource in ckan_dataset['resources']:
+            data = {'id': resource['id'], 'hash': resource.get('zh_hash', resource.get('hash', ''))
+            site.call_action('resource_patch', data, requests_kwargs={'verify': False})
+    except NotFound:
+        log.error('%s not found!' % data['id'])
